b1d75ebf7ed57b9b71f8d6c2ef8c7803d14791f6
Ventivu <3123632012@qq.com>
2021-12-12 21:39:37 +0800

    常规更新

--- src/main/java/com/ventivu/DreamAchievement/DreamAchievement.java
+++ src/main/java/com/ventivu/DreamAchievement/DreamAchievement.java
@@ -10,7 +10,7 @@ import cpw.mods.fml.common.event.FMLServerStartingEvent;
 public class DreamAchievement {
     public static final String MODID = "DreamAchievement";
     public static final String MODNAME = MODID;
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.6";
     
     @Mod.Instance(DreamAchievement.MODID)
     public static DreamAchievement instance = new DreamAchievement();
